eb8699fef9237407cb5d9e51dba6807c153c8965
Moustapha Akanmu <makanmu@spartaglobal.com>
2019-10-08 09:28:50 +0100

    (added readme file)

---
+++ README.md.py
@@ -0,0 +1,9 @@
+#TDD - Test Driven Developement
+this is a class on TDD
+
+BEnefits of TDD are:
+- Simple code
+- Atomic
+- Code to specs
+- Known when you are done ( more important for outsourcing/project work)
+- Maintanability & reduce technical debt (more importanyt for internal work)
