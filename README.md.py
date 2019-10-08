1bd1b0714821e2641acecae35c2dab14f8acacd0
Moustapha Akanmu <makanmu@spartaglobal.com>
2019-10-08 09:33:54 +0100

    (added picture to read me file)

--- README.md.py
+++ README.md.py
@@ -7,3 +7,5 @@ BEnefits of TDD are:
 - Code to specs
 - Known when you are done ( more important for outsourcing/project work)
 - Maintanability & reduce technical debt (more importanyt for internal work)
+
+![Image description](https://www.google.com/search?q=tdd&rlz=1C1CHBF_enGB866GB866&sxsrf=ACYBGNStirCL0klws-_bRygOHn7psfHOLg:1570523529434&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjA_tSIoIzlAhWSRBUIHcFxDqMQ_AUIEigB&biw=1108&bih=570&dpr=1.25#imgrc=H5NiSpjdCzHvNM:)]
